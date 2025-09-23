7f6b326b551db1d18140adc03aaffc03addfac31
krishshyara25 <krish.shyara.cg@gmail.com>
2025-09-23 13:50:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (63.25%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int total=0;
+        int total = 0;
         for(int i=0;i<s.length()-1;i++){
-            total += abs(s[i+1] - s[i]);
+            total = total + abs(s[i+1] - s[i]);
         }
         return total;
     }
