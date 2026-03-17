4c24564e23709020a77dc7d61d5eea4b0fe671b6
krishshyara25 <krish.shyara.cg@gmail.com>
2026-03-17 11:00:30 +0530

    Time: 12 ms (12.07%), Space: 71.7 MB (51.73%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        for(int i = s.size()-1;i>=0;i--){
+            if(i<s.size()-1){
+                shifts[i] = (shifts[i] + shifts[i+1])%26;
+            }
+            s[i] = (s[i] - 'a' + shifts[i]) % 26 + 'a';
+        }
+        return s;
+    }
+};
