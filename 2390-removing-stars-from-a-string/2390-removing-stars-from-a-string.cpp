79a71cb40a1fd38a62944bd6f1c776feee834d99
krishshyara25 <krish.shyara.cg@gmail.com>
2026-03-27 11:33:35 +0530

    Time: 12 ms (84.25%), Space: 28.2 MB (86.81%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string st = "";
+        for(int i = 0;i<s.size();i++){
+            if(s[i] != '*'){
+                st.push_back(s[i]);
+            }else{
+                st.pop_back();
+            }
+        }
+        return st;
+
+
+    }
+};
