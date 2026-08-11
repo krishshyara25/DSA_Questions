dc58572fc68026b0b3d6bedadf975a39da772087
krishshyara25 <krish.shyara.cg@gmail.com>
2026-08-11 10:11:18 +0530

    Time: 1 ms (0.56%), Space: 10.1 MB (27.22%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -13,10 +13,9 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
         while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
         return slow;
     }
